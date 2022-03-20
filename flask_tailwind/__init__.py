88d2aa85a465ecb2c363b4bb96fda630c6c23702
jazzthief <mynameisyegor@gmail.com>
2022-03-21 01:40:26 +0300

    Restructure to use factory method & blueprints

--- flask_tailwind/__init__.py
+++ flask_tailwind/__init__.py
@@ -1,20 +1,54 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_assets import Environment, Bundle
+from flask import render_template
 
+# from flask_assets import Environment, Bundle
 
 __version__ = '0.1.0'
-app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
-# assets = Environment()
 
-""" if os.environ.get('FLASK_ENV') == 'development':
-    app.config.from_object('config.DevelopmentConfig')
-elif os.environ.get('FLASK_ENV') == 'testing':
-    app.config.from_object('config.TestingConfig')
-else:
-    app.config.from_object('config.ProductionConfig') """
+def create_app(test_config=None):
+    """Create and configure flask app"""
 
-import flask_tailwind.routes
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_object(os.environ['APP_SETTINGS'])
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # db = SQLAlchemy(app)
+    # assets = Environment()
+
+    register_blueprints(app)
+    register_error_handlers(app)
+
+    return app
+
+
+# Helper functions
+def register_blueprints(app):
+    """Register flask app blueprints"""
+
+    from flask_tailwind.main import main_blueprint
+
+    app.register_blueprint(main_blueprint)
+
+
+def register_error_handlers(app):
+    """Register error handlers"""
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
--- flask_tailwind/models.py
+++
@@ -1,3 +0,0 @@
-from flask_tailwind import db
-
-
